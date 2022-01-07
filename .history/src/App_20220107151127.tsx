5c50f414d4885c76eca25782bf4825f95b9e42d8
niltonmalves <alves2111@hotmail.com>
2022-01-07 16:12:52 -0300

    :tada: End

---
+++ .history/src/App_20220107151127.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+import { Button } from './components/Button';
+import { MovieCard } from './components/MovieCard';
+
+// import { SideBar } from './components/SideBar';
+// import { Content } from './components/Content';
+
+import { api } from './services/api';
+
+import './styles/global.scss';
+
+import './styles/sidebar.scss';
+import './styles/content.scss';
+import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
+
+
+
+export function App() {
+  
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <>
+        <SideBar />
+      </>
+   
+      <>
+        <Content />
+      </>
+      
+      
+    </div>
+  )
+}
---
+++ .history/src/components/Content_20220107145101.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+
+import { Button } from './Button';
+import { MovieCard } from './MovieCard';
+
+// import { SideBar } from './components/SideBar';
+// import { Content } from './components/Content';
+
+import { api } from '../services/api';
+
+import '../styles/global.scss';
+
+import '../styles/sidebar.scss';
+import '../styles/content.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+
+export function Content() {
+  // Complete aqui
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+  return(
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
+}
